bd43491052d7aeffb965ee84cc5a036a36445c5a
haseeb vc wala <161650353+haseebvc@users.noreply.github.com>
2024-12-29 03:13:11 +0500

    Add files via upload

---
+++ miles-stone-1/logic.js
@@ -0,0 +1,10 @@
+var click = document.getElementById("button");
+var skills = document.getElementById("skills");
+click.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
